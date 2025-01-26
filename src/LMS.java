ee01d76f935b257f08c9af64a4dde0b26a2eb2a5
jacob <jacob-whitney@outlook.com>
2025-01-26 13:54:23 -0800

    Add test functions to LMS.java

--- src/LMS.java
+++ src/LMS.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 
+/**
+ * Jacob Whitney
+ * Software Development I
+ * January 26, 2025
+ * LMS.java
+ * Main class to manage the Library Management System,
+ * allowing users to read text files with patron information, display a list
+ * of patrons, display a menu for user interaction, create new patron
+ * records, and remove patron records.
+ */
 public class LMS {
     public static void main(String[] args) {
         // Tests (to be removed)
-        //TestPatron.testConGetSet();
-        //TestPatron.testDelete();
+        Patron jim = TestPatron.testCon();
+        TestPatron.testGetSet(jim);
+        TestPatron.testDelete(jim);
     }
 }
