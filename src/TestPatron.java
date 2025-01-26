a6b047e6b07445744b262270549316db27e5a483
jacob <jacob-whitney@outlook.com>
2025-01-26 13:55:35 -0800

    Add test methods to TestPatron.java

--- src/TestPatron.java
+++ src/TestPatron.java
@@ -1,46 +1,75 @@
+/**
+ * Jacob Whitney
+ * Software Development I
+ * January 26, 2025
+ * TestPatron.java
+ * Stores methods for testing the Patron class.
+ */
 public class TestPatron {
-    public static void testConGetSet() {
-        // Can I create Patron objects and print them?
-        Patron jim = new Patron(
+    /**
+     * method: testCon
+     * parameters: none
+     * return: Patron object
+     * purpose: test that Patron constructor is working as expected
+     */
+    public static Patron testCon() {
+        return new Patron(
                 "123",
                 "jim part",
                 "123 address",
                 65.22
         );
-        System.out.println(jim.getID());
-        jim.setID("456");
-        System.out.println(jim.getID());
-
-        System.out.println(jim.getName());
-        jim.setName("dim mart");
-        System.out.println(jim.getName());
-
-        System.out.println(jim.getAddress());
-        jim.setAddress("789 place");
-        System.out.println(jim.getAddress());
-
-        System.out.println(jim.getFines());
-        jim.setFines(123.50);
-        System.out.println(jim.getFines());
     }
 
-    public static void testDelete() {
-        // Can I delete Patron objects
-        Patron pam = new Patron(
-                "123",
-                "pam art",
-                "123 address",
-                65.22
-        );
-        System.out.println(pam.getID());
-        System.out.println(pam.getName());
-        System.out.println(pam.getAddress());
-        System.out.println(pam.getFines());
-        pam = null;
+    /**
+     * method: testGetSet
+     * parameters: patron
+     * return: none
+     * purpose: test Patron class getters and setters
+     */
+    public static void testGetSet(Patron patron) {
+        System.out.println(patron.getID());
+        System.out.println(patron.getName());
+        System.out.println(patron.getAddress());
+        System.out.println(patron.getFines());
+        System.out.println();
 
-        System.out.println(pam.getID());
-        System.out.println(pam.getName());
-        System.out.println(pam.getAddress());
-        System.out.println(pam.getFines());
+        patron.setID("456");
+        System.out.println(patron.getID());
+
+        patron.setName("dim mart");
+        System.out.println(patron.getName());
+
+        patron.setAddress("789 place");
+        System.out.println(patron.getAddress());
+
+        patron.setFines(123.50);
+        System.out.println(patron.getFines());
+    }
+
+    /**
+     * method: testDelete
+     * parameters: patron
+     * return: none
+     * purpose: test deleting a patron object
+     */
+    public static void testDelete(Patron patron) {
+        System.out.println(patron.getID());
+        System.out.println(patron.getName());
+        System.out.println(patron.getAddress());
+        System.out.println(patron.getFines());
+        patron = null;
+
+        if (patron == null) System.out.println("Object is deleted");
+    }
+
+    /**
+     * method: testPrintObject
+     * parameters: patron
+     * return: none
+     * purpose: test printing a Patron object
+     */
+    public static void testPrintObject(Patron patron) {
+
     }
 }
